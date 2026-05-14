610949bb42b41c69ef0446c74f40a8d38094f654
Gustavo Neliz <gustavopvp2006@gmail.com>
2026-05-13 22:02:21 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
+// =========================
+// ARQUIVO: script.js
+// =========================
 
+const buttons = document.querySelectorAll(".produto-card button");
+
+buttons.forEach((button) => {
+
+  button.addEventListener("click", () => {
+
+    button.innerHTML = "Adicionado ✓";
+
+    button.style.background = "#7bf1a8";
+
+    setTimeout(() => {
+
+      button.innerHTML = "Adicionar ao Carrinho";
+
+      button.style.background = "#d8b4fe";
+
+    }, 2000);
+
+  });
+
+});
+
+
+// NEWSLETTER
+
+const newsletterBtn = document.querySelector(".newsletter button");
+
+newsletterBtn.addEventListener("click", () => {
+
+  alert("Obrigado por se cadastrar na Morpheus Company!");
+
+});
