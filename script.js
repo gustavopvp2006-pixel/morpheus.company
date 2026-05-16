e3fbd249ff1afe0e25d864a44bc4380781278abc
Gustavo Neliz <gustavopvp2006@gmail.com>
2026-05-15 21:25:23 -0300

    Update script.js

--- script.js
+++ script.js
@@ -568,3 +568,18 @@ window.addEventListener("scroll", () => {
   }
 
 });
+// FAQ ABRIR / FECHAR
+
+const faqItems = document.querySelectorAll(".faq-item");
+
+faqItems.forEach(item => {
+
+    const question = item.querySelector(".faq-question");
+
+    question.addEventListener("click", () => {
+
+        item.classList.toggle("active");
+
+    });
+
+});
